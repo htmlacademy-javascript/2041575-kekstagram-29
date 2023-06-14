24c527c8a742bd40d5f9093b66a20510f471ddec
Mikhail <mihancho5340@gmail.com>
2023-06-14 11:12:05 +0600

    Сделал доп задание

--- js/functions.js
+++ js/functions.js
@@ -15,12 +15,11 @@ compareStringLength(SOME_STRING, EQUAL_VALUE);
 
 //Функция для проверки, является ли строка палиндромом
 
-const ANOTHER_STRING = 'за мо к           о ма з';
+const ANOTHER_STRING = 'за мо к           о ма З';
 
 function isStringPolydrom(string) {
   let comparingString = '';
-
-  string = string.replaceAll(' ', '');
+  string = string.replaceAll(' ', '').toLowerCase();
 
   for (let i = string.length - 1 ; i >= 0 ; i--) {
     comparingString += string.at(i);
@@ -34,3 +33,37 @@ function isStringPolydrom(string) {
 }
 
 isStringPolydrom(ANOTHER_STRING);
+
+
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+// Если в строке нет ни одной цифры, функция должна вернуть NaN:
+// Если хотите усложнить задание, предусмотрите случай, когда вместо строки приходит число.
+// Обратите внимание, что возвращать функция по-прежнему должна только целые положительные числа:
+
+const NEW_STRING = '354657';
+
+function getIntNumber(value) {
+  let resultString = '';
+
+  for (let i = 0 ; i < value.length ; i++) {
+    if (isNaN(parseInt(value[i], 10))) {
+      for(let j = 0 ; j < value.length ; j++) {
+        value[j] = parseInt(value[j], 10);
+        if (!isNaN(value[j])) {
+          resultString += value[j];
+        }
+      }
+
+      if (isNaN(parseInt(resultString, 10))) {
+        return NaN;
+      }
+      return parseInt(resultString, 10);
+
+    } else {
+      resultString += value[i];
+    }
+  }
+  return parseInt(resultString, 10);
+}
+
+getIntNumber(NEW_STRING);
